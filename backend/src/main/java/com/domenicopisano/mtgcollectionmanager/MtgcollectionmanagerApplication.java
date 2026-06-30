e14d90243f31b9170ef7dd6354b7d84333ca57dc
Domenico Pisano <domenicopisano123@gmail.com>
2026-06-30 11:09:47 +0200

    chore: create Spring Boot backend

---
+++ backend/src/main/java/com/domenicopisano/mtgcollectionmanager/MtgcollectionmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.domenicopisano.mtgcollectionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgcollectionmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgcollectionmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/domenicopisano/mtgcollectionmanager/MtgcollectionmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domenicopisano.mtgcollectionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgcollectionmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
